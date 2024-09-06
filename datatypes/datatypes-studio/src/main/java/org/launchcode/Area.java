d63b4a80d7c72c54239a2ebd9e26137c892ec014
himanlaunch <himanshureddyk1995@gmail.com>
2024-09-05 20:13:58 -0500

    hi

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,23 @@
+package org.launchcode;
+
+import studios.areaofacircle.Circle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main (String[] Args){
+        Scanner input;
+        // double pi = ;
+        double radius;
+        double area;
+
+        input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        radius = input.nextDouble();
+        // area = Math.PI * Math.pow(radius,2);
+        area = Circle.getArea(radius);
+        System.out.println("The area of a circle of radius " + radius + " is: " + area + "cm²");
+
+        input.close();
+    }
+}
---
+++ datatypes/datatypes-studio/src/main/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,7 @@
+package studios.areaofacircle;
+
+public class Circle {
+    public static double getArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+}
